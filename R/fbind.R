70c66f99dc750e97f4ead41974c7822718f6ce8d
Maria Paula Caldas <mpaulacaldas@gmail.com>
2019-07-09 10:05:39 +0200

    document()

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,18 @@
+#' Bind factors
+#'
+#' Combines factors, like you would expect.
+#'
+#' @param x First factor.
+#' @param y Second factor.
+#'
+#' @return A new factor, with the levels of `x` and `y`.
+#' @export
+#'
+#' @examples
+#' a <- factor(c("hola", "hello", "salut"))
+#' b <- factor(c("adios", "bye", "salut"))
+#' fbind(a, b)
+#'
 fbind <- function(x, y) {
   factor(c(as.character(x), as.character(y)))
 }
